17a7069ff579b0a04efc1883dc117a2c256770d5
Regis de Toni <regis@mura.com.br>
2025-02-16 20:27:54 -0300

    Sample scene with malicious detect image

--- block-scripts-unity3d/Assets/Scripts/DetectScriptsManager.cs
+++ block-scripts-unity3d/Assets/Scripts/DetectScriptsManager.cs
@@ -1,13 +1,21 @@
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using System.Security.Cryptography;
 
-namespace DetectScripts
-{
+namespace DetectScripts{
 
-    public class DetectScriptsManager : MonoBehaviour
-    {
+    public struct ScriptStatus{
+        public string sha256;
+        public bool isMalicious;
+        public bool exist;
+    }
+
+    public class DetectScriptsManager : MonoBehaviour{
 
         public ScriptDetector[] scriptDetectors;
+
+        public GameObject blockInteraction;
 
         void Start()
         {
@@ -16,21 +24,44 @@ namespace DetectScripts
 
         void DetectScripts()
         {
-            foreach (var detector in this.scriptDetectors)
-            {
+            foreach (var detector in scriptDetectors){
                 detector.DetectScripts();
                 Dictionary<string, string> scripts = detector.GetScriptsDetected();
-                foreach (var script in scripts)
-                {
-                    Debug.Log("Script detected: " + script.Key);
-                    Debug.Log("Script content: " + script.Value);
+                foreach (var script in scripts){
+                    ScriptStatus status = GetScriptStatus(script.Value);
+                    if(status.isMalicious){
+                        blockInteraction.SetActive(true);
+                        Debug.Log("Malicious script detected: " + script.Key);
+                    }
                 }
             }
         }
 
-        void SendScriptsToServer(string scriptValue){
-            // Send the script to the server
+        ScriptStatus GetScriptStatus(string scriptValue){
+            string sha256Script = sha256(scriptValue);
+            ScriptStatus scriptStatus = GetStatusFromServer(sha256Script);
+            return scriptStatus;
         }
+
+        ScriptStatus GetStatusFromServer(string sha256Script){
+            ScriptStatus status = new ScriptStatus();
+            status.sha256 = sha256Script;
+            status.isMalicious = true;
+            status.exist = true;
+            return status;
+        }
+
+        public string sha256(string randomString){
+            var crypt = new SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
     }
 
 
